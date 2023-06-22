900b7557c87f026882b55294a1da289caeab75ca
Kaitesi_Joan <joankaitesi01@gmail.com>
2023-06-22 07:54:30 +0000

    changed the color of the appbar

---
+++ lib/body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("hellon bit retiu", CircularProgressIndicator()),
+    );
+  }
+}
